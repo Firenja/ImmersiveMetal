2f3e0f614852954c5e4e4bf4c4f8adad8caba9d0
Vestri <p.prokop@firenja.com>
2020-05-18 11:32:08 +0200

    Ore and pickaxe harvest level changed

--- src/main/java/com/immersivemetal/list/ToolMaterialList.java
+++ src/main/java/com/immersivemetal/list/ToolMaterialList.java
@@ -15,17 +15,17 @@ public enum ToolMaterialList implements IItemTier
     Hard_iron(6,10,500,5,25,ItemList.hard_ironIngot),
     Mithril(6,10,1200,6,25,ItemList.mithrilIngot),
     Orichalcum(6,10,1000,7,25,ItemList.orichalcumIngot),
-    Adamant(6,10,1400,8,25,ItemList.adamantIngot),
-    Glowing_iron(7,10,900,9,35,ItemList.glowing_ironIngot),
-    Uridium(6,10,1100,10,35,ItemList.uridiumIngot),
-    Tritanium(8,10,1300,11,35,ItemList.tritaniumIngot),
-    Quadium(8,10,1600,12,35,ItemList.quadiumIngot),
-    Etherium(8,10,1900,13,35,ItemList.etheriumIngot),
-    Byzanium(9,10,2000,14,35,ItemList.byzaniumIngot),
-    Bolognium(9,10,2300,15,35,ItemList.bologniumIngot),
-    Duratine(9,10,2500,15,40,ItemList.duratineIngot),
-    Dark_iron(10,10,2700,16,45,ItemList.dark_ironIngot),
-    Arenak(12,10,3000,16,50,ItemList.arenakIngot);
+    Adamant(6,10,1400,7,25,ItemList.adamantIngot),
+    Glowing_iron(7,10,900,7,35,ItemList.glowing_ironIngot),
+    Uridium(6,10,1100,8,35,ItemList.uridiumIngot),
+    Tritanium(8,10,1300,9,35,ItemList.tritaniumIngot),
+    Quadium(8,10,1600,9,35,ItemList.quadiumIngot),
+    Etherium(8,10,1900,10,35,ItemList.etheriumIngot),
+    Byzanium(9,10,2000,10,35,ItemList.byzaniumIngot),
+    Bolognium(9,10,2300,11,35,ItemList.bologniumIngot),
+    Duratine(9,10,2500,11,40,ItemList.duratineIngot),
+    Dark_iron(10,10,2700,12,45,ItemList.dark_ironIngot),
+    Arenak(12,10,3000,13,50,ItemList.arenakIngot);
 
     private float attackDamage;
     private float effi;
